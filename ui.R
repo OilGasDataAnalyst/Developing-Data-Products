f813fa529857a8ea8ed1c26f716fed20c6b5d715
OilGasDataAnalyst <sethmcdonald@Seth-McDonalds-iMac.local>
2015-11-18 22:39:17 -0600

    Minor Changes
    Minor changes to the ui portion

--- ui.R
+++ ui.R
@@ -1,20 +1,22 @@
 #preventing shiny from running calcs all the time
 shinyUI(pageWithSidebar(
-  headerPanel("Calculate your BMR"),
+  headerPanel("Calculate your BMR (Basal Metabolic Rate)"),
   fluidRow(
     column(3, wellPanel(
-      numericInput(inputId="Weight", label = "Weight", value = 100, min = 0, max = 1000),
+      numericInput(inputId="Weight", label = "Please enter your weight and 
+                   select the units:", value = 100, min = 0, max = 1000),
       radioButtons("WeightChoose", "Kg or Pounds?",
                  choices = c("Kg" = "KG",
                              "Pounds" = "LBS"))
     )),
     
     column(3, wellPanel(
-      numericInput(inputId="Height", label = "Height",value = 72, min = 0, max = 500),
+      numericInput(inputId="Height", label = "Please enter your height and 
+                   select the units:",value = 72, min = 0, max = 500),
       radioButtons("HeightChoose", "Inches or CM?",
                  choices = c("Inches" = "INCH",
                              "CM" = "CM")),
-      numericInput(inputId="Age", label = "Age (Years)",value = 25, min = 1, max = 130)
+      numericInput(inputId="Age", label = "Enter your age in years:",value = 25, min = 1, max = 130)
     )),
     
     column(3, wellPanel(
@@ -26,7 +28,7 @@ shinyUI(pageWithSidebar(
   mainPanel(
     p('Weight entered in Kg:'),
     textOutput('Weight'),
-    p('Height entered in CM'),
+    p('Height entered in CM:'),
     textOutput('Height'),
     p('Your BMR value is:'),
     textOutput('BMRValue')
